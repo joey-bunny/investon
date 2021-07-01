b143c2a5a5ea3eb762ad4948813a21fa21204c5e
joey-bunny <joecliqs@gmail.com>
2021-07-01 04:23:30 +0100

    Refactored auth route 3

--- Routes/auth.js
+++ Routes/auth.js
@@ -18,7 +18,7 @@ const max = 100000000;
 const code = Math.floor(Math.random() * (max - min + 1)) + min;
 
 router.get('', async (req, res) => {
-    return res.send('welcome')
+    return res.send(process.env.BASE_URL)
 })
 
 /*
